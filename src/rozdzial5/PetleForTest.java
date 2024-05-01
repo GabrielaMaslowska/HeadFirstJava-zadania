9fad3dcd4d716512525b09b2fe2b77149d4c4697
GabrielaMaslowska <mgabrielamaslowska@gmail.com>
2024-05-01 10:11:55 +0200

    update

--- src/rozdzial5/PetleForTest.java
+++ src/rozdzial5/PetleForTest.java
@@ -1,2 +1,14 @@
-package rozdzial5;public class PetleForTest {
+package rozdzial5;
+
+public class PetleForTest {
+    public static void main(String[] args) {
+            for (int x = 0; x < 4; x++) {
+                for (int y = 4; y > 2; y--) {
+                    System.out.println(x + " " + y);
+                }
+                if (x == 1) {
+                    x++;
+                }
+        }
+    }
 }
