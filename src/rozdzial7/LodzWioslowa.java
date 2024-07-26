572b5528b07a6cd6b914e64c12253dd535f9ff65
GabrielaMaslowska <mgabrielamaslowska@gmail.com>
2024-07-26 21:33:24 +0200

    update

--- src/rozdzial7/LodzWioslowa.java
+++ src/rozdzial7/LodzWioslowa.java
@@ -1,4 +1,38 @@
 package rozdzial7;
 
-public class LodzWioslowa {
+public class LodzWioslowa _ extends {
+    public _ wioslowanie() {
+        System.out.println("wiosłuj Natasza");
+    }
+}
+
+public class _ {
+    private int _;
+    _ void _ (_) {
+        dlugosc = dl;
+    }
+    public int getDlugosc() {
+        _ _;
+    }
+    public _ plyn() {
+        System.out.print("_");
+    }
+}
+
+public class TestLodzi {
+    _ _ _ main(String[] args) {
+        _ b1 = new Lodz();
+        LodzZaglowa b2 = new _ ();
+        LodzWioslowa _ = new LodzWioslowa();
+        b2.setDlugosc(32);
+        b1._();
+        b3._();
+        _.plyn();
+    }
+}
+
+public class _ _ Lodz {
+    public _ _ () {
+        System.out.print("_");
+    }
 }
